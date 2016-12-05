571bbc506477b5357bfcd5b96f2c82351b2756e2
Sean Warren <s.warren@garvan.org.au>
2016-12-05 18:34:42 +1100

    Build directly into Build/ folder

--- build-flim-ui.bat
+++ build-flim-ui.bat
@@ -4,7 +4,7 @@ IF NOT DEFINED MSVC_VER SET MSVC_VER=14
 if %MSVC_VER%==14 SET MSVC_YEAR=2015
 
 echo Cleaning CMake Project
-SET PROJECT_DIR=Build\MSVC%MSVC_VER%_64
+SET PROJECT_DIR=Build\
 rmdir %PROJECT_DIR% /s /q
 mkdir %PROJECT_DIR%
 cd %PROJECT_DIR%
@@ -12,7 +12,7 @@ cd %PROJECT_DIR%
 set GENERATOR="Visual Studio %MSVC_VER% %MSVC_YEAR% Win64"
 echo Generating CMake Project in: %PROJECT_DIR%
 echo Using Generator: %GENERATOR%
-cmake -G %GENERATOR% ..\..\Source\
+cmake -G %GENERATOR% ..\Source\
 
 echo Building 64bit Project in Release mode
 cmake --build . --config Release --target PACKAGE
