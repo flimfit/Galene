ec084095cf98bd96ae80e117096eac8820b7834d
Sean Warren <s.warren@garvan.org.au>
2017-05-09 21:09:02 +1000

    Fix deployment on Mac

--- build-flim-ui.sh
+++ build-flim-ui.sh
@@ -2,12 +2,9 @@
 
 export CC=/usr/local/opt/llvm/bin/clang
 export CXX=/usr/local/opt/llvm/bin/clang++
-export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib -L/usr/local/opt/opencv3/lib"
+export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
 export OSX_SDK_VERSION=10.12
 
-#export CC=/usr/local/bin/gcc-6
-#export CXX=/usr/local/bin/g++-6
-#export MACOSX_DEPLOYMENT_TARGET=10.10.0
 export PATH="/usr/local/opt/qt5/bin:$PATH"
 
 export MACOSX_DEPLOYMENT_TARGET=10.12
@@ -15,3 +12,7 @@ export MACOSX_DEPLOYMENT_TARGET=10.12
 cmake -G"Unix Makefiles" -HSource -BBuild
 
 cmake --build Build
+
+# fix up the half-ass job done by macdeployqt
+# see: https://github.com/iltommi/macdeployqtfix/blob/master/macdeployqtfix.py
+python macdeployqtfix.py Build/flim-ui/flim_ui.app/Contents/MacOS/flim_ui /usr/local/opt/qt5
