d7c100a642a1aa55fa1d60929535b925f5855d03
Sean Warren <s.warren@garvan.org.au>
2017-06-14 09:42:35 +1000

    Use new merged master branch on FlimReader

--- build-flim-ui.sh
+++ build-flim-ui.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+# Build script for Galene on Mac. 
+# Requires: 
+#    - MacOS >=10.12
+#    - Qt >=5.7
+#    - Up-to-date LLVM (i.e. not supplied by Apple)
+# Currently assumes these are installed by brew, i.e. using 
+#    - brew install qt5 llvm
+
+# Set manual locations for clang, Qt. 
+# Override these if not installed via brew
 export CC=/usr/local/opt/llvm/bin/clang
 export CXX=/usr/local/opt/llvm/bin/clang++
 export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
-export OSX_SDK_VERSION=10.12
-
 export PATH="/usr/local/opt/qt5/bin:$PATH"
 
+# Set deployment target
+export OSX_SDK_VERSION=10.12
 export MACOSX_DEPLOYMENT_TARGET=10.12
 
+# Generate make files and build 
 cmake -G"Unix Makefiles" -HSource -BBuild
-
-cmake --build Build
+cmake --build Build -- -j
 
 # fix up the half-ass job done by macdeployqt
 # see: https://github.com/iltommi/macdeployqtfix/blob/master/macdeployqtfix.py
