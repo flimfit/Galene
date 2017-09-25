0c09427e0819634d8c60095e9bf4eeabb8e3021b
Sean Warren <s.warren@garvan.org.au>
2017-09-25 14:23:20 +1000

    Update fifo-flim

--- Source/flim-ui/LifetimeDisplayWidget.h
+++ Source/flim-ui/LifetimeDisplayWidget.h
@@ -8,7 +8,7 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 
-class LifetimeDisplayWidget : public QWidget, public Ui::LifetimeDisplayWidget, private ControlBinder //, public ImageDataSourceWatcher
+class LifetimeDisplayWidget : public QWidget, public Ui::LifetimeDisplayWidget, private ControlBinder
 {
    Q_OBJECT
 
