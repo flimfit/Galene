6776231d55f082bd880016787bbb20088fe8b5c2
Sean Warren <s.warren@garvan.org.au>
2017-09-28 09:21:07 +1000

    Integrate ome-files on mac (no longer using hunter)

--- build-flim-ui.sh
+++ build-flim-ui.sh
@@ -11,18 +11,37 @@
 
 # Set manual locations for clang, Qt. 
 # Override these if not installed via brew
+export OME_FILES_ROOT=$(pwd)/ome-files-install
 export CC=/usr/local/opt/llvm/bin/clang
 export CXX=/usr/local/opt/llvm/bin/clang++
 export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
-export PATH="/usr/local/opt/qt5/bin:$PATH"
+export PATH="$OME_FILES_ROOT/lib:/usr/local/opt/qt5/bin:$PATH"
 export MACOSX_DEPLOYMENT_TARGET=10.10
+export DYLIB_LIBRARY_PATH="$OME_FILES_ROOT/lib"
 
-cmake -GNinja -HSource/ome-cmake-superbuild -Bome-files-build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=ome-files-install -Dsource-cache="cache/source/" -Dhead:BOOL=ON -Dtest:BOOL=OFF -Dextended-tests:BOOL=OFF -Dqtgui:BOOL=OFF
-cmake --build ome-files-build
+#brew install --with-clang llvm qt cmake libtiff libpng opencv qt5 boost xerces-c xalan-c
+
+if [ ! -d "ome-files-install" ]; then
+   cmake -GNinja -Home-cmake-superbuild -Bome-files-build -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=ome-files-install \
+      -Dsource-cache="cache/source/" \
+      -Dhead:BOOL=ON \
+      -Dtest:BOOL=OFF \
+      -Dextended-tests:BOOL=OFF \
+      -Dqtgui:BOOL=OFF \
+      -Dome-cmake-superbuild_USE_SYSTEM_tiff:BOOL=ON \
+      -Dome-cmake-superbuild_USE_SYSTEM_png:BOOL=ON \
+      -Dome-cmake-superbuild_USE_SYSTEM_bzip2:BOOL=ON \
+      -Dome-cmake-superbuild_USE_SYSTEM_boost:BOOL=ON \
+      -Dome-cmake-superbuild_USE_SYSTEM_xalan:BOOL=ON \
+      -Dome-cmake-superbuild_USE_SYSTEM_xerces:BOOL=ON 
+   cmake --build ome-files-build --target install
+fi
 
 # Generate make files and build 
-cmake -GNinja -HSource -BBuild -DCMAKE_BUILD_TYPE=Release
-cmake --build Build
+rm -rf Build
+cmake -GNinja -HSource -BBuild -DCMAKE_BUILD_TYPE=Debug
+cmake --build Build  # --clean-first
 
 # sign code (requires that signature is installed in keychain)
 codesign --verbose --deep -s P6MM899VL9 Build/flim-ui/Galene.app/
