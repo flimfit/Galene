e03469c5cc54ae4b18cb70559fe45c27128f2f8c
Sean Warren <s.warren@garvan.org.au>
2017-10-29 21:27:59 +1100

    Move ome-files build into lib folder

--- build-flim-ui.sh
+++ build-flim-ui.sh
@@ -6,7 +6,7 @@
 #    - CMake >= 3.7
 #    - Qt >=5.7
 # Currently assumes these are installed by brew, i.e. using 
-#    - brew install qt cmake
+#    - brew install qt cmake opencv boost xerces-c xalan-c
 
 # Set manual locations for clang, Qt. 
 # Override these if not installed via brew
@@ -14,21 +14,6 @@ export OME_FILES_ROOT=$(pwd)/ome-files-install
 export PATH="$OME_FILES_ROOT/lib:/usr/local/opt/qt5/bin:$PATH"
 export DYLIB_LIBRARY_PATH="$OME_FILES_ROOT/lib"
 export CMAKE_PREFIX_PATH=$(pwd)/lib-install
-
-#brew install --with-clang llvm qt cmake opencv boost xerces-c xalan-c dlib
-
-if [ ! -d "ome-files-install" ]; then
-   cmake -GNinja -Home-cmake-superbuild -Bome-files-build -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX=ome-files-install \
-      -Dsource-cache="cache/source/" \
-      -Dhead:BOOL=ON \
-      -Dtest:BOOL=OFF \
-      -Dextended-tests:BOOL=OFF \
-      -Dqtgui:BOOL=OFF \
-      -Dbuild-prerequisites:BOOL=OFF ^
-      -Dbuild-packages=ome-common;ome-model;ome-files ^
-   cmake --build ome-files-build --target install
-fi
 
 # Generate make files and build 
 BUILD_TYPE=Debug
--- build-libs.sh
+++ build-libs.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
-# Generate make files and build 
-BUILD_TYPE=Debug
-BUILD_DIR=lib-build/bioimageconvert_$BUILD_TYPE
-rm -rf $BUILD_DIR
-cmake -GNinja -Hlib/bioimageconvert -B$BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+# Build BioImage
+#========================
+BUILD_DIR=lib-build/bioimageconvert
+#rm -rf $BUILD_DIR
+cmake -GNinja -Hlib/bioimageconvert -B$BUILD_DIR -DCMAKE_BUILD_TYPE=Release \
    -DBIC_ENABLE_FFMPEG=OFF \
    -DBIC_ENABLE_GDCM=OFF \
    -DBIC_ENABLE_GDCM=OFF \
@@ -16,8 +15,22 @@ cmake -GNinja -Hlib/bioimageconvert -B$BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE 
    -DBIC_ENABLE_QT=OFF \
    -DBIC_ENABLE_OPENCV=ON \
    -DBIC_ENABLE_OPENMP=OFF \
-   -DBIC_ENABLE_IMGCNV=ON \
+   -DBIC_ENABLE_IMGCNV=OFF \
    -DLIBBIOIMAGE_TRANSFORMS=ON \
    -DCMAKE_INSTALL_PREFIX=lib-install
 
-cmake --build $BUILD_DIR --target install # --clean-first
+cmake --build $BUILD_DIR --target install
+
+# Build ome-files
+#========================
+cmake -GNinja -Hlib/ome-cmake-superbuild -Blib-build/ome-files -DCMAKE_BUILD_TYPE=Release \
+   -DCMAKE_INSTALL_PREFIX=lib-install \
+   -Dsource-cache="lib/cache/source/" \
+   -Dhead:BOOL=ON \
+   -Dtest:BOOL=OFF \
+   -Dextended-tests:BOOL=OFF \
+   -Dqtgui:BOOL=OFF \
+   -Dbuild-prerequisites:BOOL=OFF \
+   -Dbuild-packages=ome-common\;ome-model\;ome-files
+
+cmake --build lib-build/ome-files --target install
