9920ea5fb9e56d861a34033a16ae0aeac7b4e907
Sean Warren <s.warren@garvan.org.au>
2017-11-17 14:50:32 +1100

    Automatically enter visual studio console

--- build-libs.bat
+++ build-libs.bat
@@ -3,14 +3,16 @@
 REM =====================================
 REM Install the following packages wth vcpkg and chocolatey before running this script:
 REM vcpkg install opencv:x64-windows fftw3:x64-windows boost:x64-windows xalan-c:x64-windows exiv2:x64-windows libraw:x64-windows eigen3:x64-windows proj4:x64-windows hdf5:x64-windows
-REM choco install cmake python2 pip nsis -y
+REM choco install cmake python2 pip nsis ninja -y
 REM pip install genshi 
 REM =====================================
 
-REM Execute this command first to switch to VS2017 console, otherwise Ninja gets confused
-REM "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-
 SETLOCAL
+
+set CURDIR=%cd%
+REM : Change this path if using a different version of Visual Studio 
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+cd %CURDIR%
 
 SET PATH=%PATH%;%VCPKG_ROOT%\installed\x64-windows\bin;%VCPKG_ROOT%\installed\x64-windows\debug\bin
 SET TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
