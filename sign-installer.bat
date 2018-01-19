ec1872a0bc2b34911bf0debdd45ce06a11ef8caf
Sean Warren <s.warren@garvan.org.au>
2018-01-20 09:33:29 +1100

    Use new card-based code signing on windows

--- sign-installer.bat
+++ sign-installer.bat
@@ -1,4 +1,3 @@
 @echo off
 echo Signing Installer File
-echo CERTFILE=%CERTFILE%
-for %%f in (.\Build\*.exe) do "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /debug /v /f "%CERTFILE%" /p "%CERTPASS%" /n "Sean Warren" /tr http://timestamp.globalsign.com/?signature=sha2 /td sha256 "%%f"
+for %%f in (.\Build\*.exe) do "C:\Program Files (x86)\Windows Kits\10\bin\x86\signtool" sign /debug /v /n "Sean Warren" /tr http://timestamp.globalsign.com/?signature=sha2 /td sha256 "%%f"
