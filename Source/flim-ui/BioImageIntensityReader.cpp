49998c627a2501d742f34317d7ac8ab8f349d793
Sean Warren <s.warren@garvan.org.au>
2018-06-17 12:32:09 +1000

    Support raw tif using libbioimage

--- Source/flim-ui/BioImageIntensityReader.cpp
+++ Source/flim-ui/BioImageIntensityReader.cpp
@@ -7,13 +7,12 @@ BioImageIntensityReader::BioImageIntensityReader(const std::string& filename) :
    // Create TIFF reader
    image5d = std::make_shared<bim::Image5D>();
    image5d->fromFile(filename);
-
    readMetadata();
 }
 
 QStringList BioImageIntensityReader::supportedExtensions()
 {
-   return { "lsm" };
+   return { "lsm", "tif" };
 }
 
 
@@ -26,6 +25,15 @@ void BioImageIntensityReader::readMetadata()
    n_z = image5d->numberZ();
    n_t = image5d->numberT();
    n_chan = image5d->numberC();
+
+   swap_zt = (n_t == 1 && n_z > 1);
+   if (swap_zt)
+   {
+      int nb = n_t;
+      n_t = n_z;
+      n_z = nb;
+      std::cout << "Swapping z and t\n";
+   }
    
    setUseAllChannels();   
    scan_params = ImageScanParameters(100, 101, 101 * (n_y + 1), n_x, n_y, n_z, bidirectional);
@@ -41,7 +49,7 @@ void BioImageIntensityReader::addStack(int chan, int t, cv::Mat& data)
    {
       try
       {
-         auto im = image5d->imageAt(t, z);
+         auto im = swap_zt ? image5d->imageAt(z, t) : image5d->imageAt(t, z);
          cv::Mat cvbuf = im->asCVMat(chan);         
          cvbuf.convertTo(cv16, CV_16U);
 
