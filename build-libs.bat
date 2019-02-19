fce9105809d6bf2888251e2b205c6b6a4cda9a96
Sean Warren <s.warren@garvan.org.au>
2019-02-20 10:24:42 +1100

    Check for Visual studio Community and build tools

--- build-libs.bat
+++ build-libs.bat
@@ -9,12 +9,17 @@ REM =====================================
 
 SETLOCAL
 
-set CURDIR=%cd%
-REM : Change this path if using a different version of Visual Studio 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-cd %CURDIR%
+SET VSCOMMUNITYCMD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+SET VSBUILDCMD="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
 
-SET PATH=%PATH%;%VCPKG_ROOT%\installed\x64-windows\bin;%VCPKG_ROOT%\installed\x64-windows\debug\bin
+:: Set up Visual Studio environment variables
+IF EXIST %VSCOMMUNITYCMD% CALL %VSCOMMUNITYCMD% -arch=amd64 && GOTO :BUILD
+IF EXIST %VSBUILDCMD% CALL %VSBUILDCMD% -arch=amd64 && GOTO :BUILD
+ECHO Error: Visual Studio install not found && EXIT /B 1
+
+:BUILD
+SET TRIPLET=x64-windows
+SET PATH=%PATH%;%VCPKG_ROOT%\installed\%TRIPLET%\bin;%VCPKG_ROOT%\installed\%TRIPLET%\debug\bin
 SET TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
 SET TOOLCHAIN_FILE=%TOOLCHAIN_FILE:\=/%
 SET BUILD_FLAGS=-DCMAKE_TOOLCHAIN_FILE=%TOOLCHAIN_FILE%
@@ -25,10 +30,10 @@ mkdir lib\install
 IF NOT "%1"=="--build-debug" GOTO build_release
 
 cmake -GNinja -Hlib -Blib/build/RelWithDebInfo -DCMAKE_BUILD_TYPE=RelWithDebInfo %BUILD_FLAGS%
-echo %ERRORLEVEL%
-if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
+ECHO %ERRORLEVEL%
+IF %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
 cmake --build lib/build/RelWithDebInfo
-if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
+IF %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
 
 cmake -GNinja -Hlib -Blib/build/Debug -DCMAKE_BUILD_TYPE=Debug %BUILD_FLAGS%
 if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
