27a314827f8853884fdba12ad2b91ce6de07eae9
Sean Warren <s.warren@garvan.org.au>
2019-02-21 17:25:19 +1100

    Use omefiles from vcpkg

--- build-flim-ui.bat
+++ build-flim-ui.bat
@@ -2,13 +2,14 @@
 
 SETLOCAL
 
-SET PATH=%PATH%;%VCPKG_ROOT%\installed\x64-windows\bin;%VCPKG_ROOT%\installed\x64-windows\debug\bin
+SET TRIPLET=x64-windows
+SET PATH=%PATH%;%VCPKG_ROOT%\installed\%TRIPLET%\bin;%VCPKG_ROOT%\installed\%TRIPLET%\debug\bin
 SET TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
 SET TOOLCHAIN_FILE=%TOOLCHAIN_FILE:\=/%
+SET OME_FILES_ROOT=%VCPKG_ROOT%\installed\%TRIPLET%
 
 SET LIB_INSTALL_PATH=%cd%\lib\install
 SET LIB_INSTALL_PATH=%LIB_INSTALL_PATH:\=/%
-SET OME_FILES_ROOT=%LIB_INSTALL_PATH%
 SET LIB_INSTALL_PATH=%LIB_INSTALL_PATH%;%LIB_INSTALL_PATH%/lib
 SET CMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH%;%LIB_INSTALL_PATH%
 
@@ -19,7 +20,7 @@ IF "%1"=="--clean" rmdir Build /s /q
 
 echo Generating CMake Project
 echo Using Generator: "Visual Studio 15 2017 Win64"
-cmake -G "Visual Studio 15 2017 Win64" -HSource -BBuild -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%" -DOME_FILES_ROOT="%OME_FILES_ROOT%"
+cmake -G "Visual Studio 15 2017 Win64" -HSource -BBuild -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN_FILE%" -DOME_FILES_ROOT="%OME_FILES_ROOT%" -DVCPKG_TARGET_TRIPLET=%TRIPLET%
 if %ERRORLEVEL% GEQ 1 EXIT /B %ERRORLEVEL%
 
 echo Building 64bit Project in Release mode
