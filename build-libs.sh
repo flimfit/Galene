b66617308de74d6e1b7f98e82fae9f94179f7694
Sean Warren <s.warren@garvan.org.au>
2019-05-08 09:00:55 +1000

    Remove old library build scripts

--- build-libs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export VCPKGROOT=~/vcpkg
-
-# use brew zlib for compatability with macOS 10.11 (provides older zlib)
-cmake -GNinja -Hlib -Blib/build -DCMAKE_TOOLCHAIN_FILE=$VCPKGROOT/scripts/buildsystems/vcpkg.cmake 
-cmake --build lib/build 
