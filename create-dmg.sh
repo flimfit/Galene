a33c1ed6c41c832d535626fbdf0f26ac62d1a802
Sean Warren <s.warren@garvan.org.au>
2019-05-21 17:15:44 +1000

    Update apple code signing

--- create-dmg.sh
+++ create-dmg.sh
@@ -7,8 +7,8 @@
 # npm install -g appdmg
 
 # sign code (requires that signature is installed in keychain)
-security unlock-keychain -p $keychain_password
-codesign --verbose --deep -s P6MM899VL9 Build/Release/flim-ui/Galene.app/
+#security unlock-keychain -p $keychain_password
+codesign --verbose --deep -s $(APPLE_CERTIFICATE_SIGNING_IDENTITY) Build/Release/flim-ui/Galene.app/
 
 GIT_DESCRIBE=$(git describe)
 appdmg Assets/appdmg.json "Galene $GIT_DESCRIBE.dmg"
