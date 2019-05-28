c813f23d0e7aae762c714b34b96172bf6fd54e53
Sean Warren <s.warren@garvan.org.au>
2019-05-28 15:39:05 +1000

    Update mac build for 10.13

--- build-flim-ui.sh
+++ build-flim-ui.sh
@@ -5,11 +5,14 @@
 #    - MacOS >=10.12
 #    - CMake >= 3.7
 
+
 # Generate make files and build 
+SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
 BUILD_TYPE=Release
 [ "$1" == "--clean" ] && rm -rf Build/$BUILD_TYPE
 cmake -GNinja -HSource -BBuild/$BUILD_TYPE \
    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 \
    -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake 
 
-cmake --build Build/$BUILD_TYPE  # --clean-first
+cmake --build Build/$BUILD_TYPE
